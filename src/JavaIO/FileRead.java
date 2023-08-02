a95f748f61c66a19737fc59b29d11545c83842e2
GoldenStone929 <yshawncui@gmail.com>
2023-08-02 11:42:01 -0700

    start testing Excel file and SDF file

--- src/JavaIO/FileRead.java
+++ src/JavaIO/FileRead.java
@@ -8,13 +8,25 @@ import java.io.*;
 public class FileRead {
     public static void main(String[] args) {
 //        String path = "/Users/sc/Java_things/src/test.txt";
-        String path = "/Users/sc/Desktop/Java_things/src/JavaIO/test.txt";
+//        String path = "/Users/sc/Desktop/Java_things/src/JavaIO/test.txt";
+          String path_Excel = "/Users/sc/Desktop/Java_things/src/JavaIO/excelFile.xlsx";
+          String path_SDF = "/Users/sc/Desktop/Java_things/src/JavaIO/sdfExample.sdf";
+
 
         try {
-            String content = new String(Files.readAllBytes(Paths.get(path)));
+            String content = new String(Files.readAllBytes(Paths.get(path_Excel)));
             System.out.println(content);
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        } // this is for Excel Output testing
+
+
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(path_SDF)));
+            System.out.println(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } // this is for SDF file read result
     }
 }
+
